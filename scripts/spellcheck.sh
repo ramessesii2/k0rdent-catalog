0e5d8acaf66b1f55ce22f6eba4dea553171c97c4
Josef Hak <jhak@mirantis.com>
2025-05-26 21:01:48 +0200

    Improve "scripts/spellcheck.sh" - test only description and summary

--- scripts/spellcheck.sh
+++ scripts/spellcheck.sh
@@ -3,13 +3,16 @@ set -euo pipefail
 
 input_dir=mkdocs
 echo "⏳ Running spell check for *.md files in 'mkdocs' folder"
-find $input_dir -type f -name "*.md" -exec hunspell -d en_US -p ./hunspell_dict.txt -l {} \; | sort | uniq > spellcheck_detected.txt
-if grep -rnwFf spellcheck_detected.txt $input_dir --include="*.md"; then
-  echo "=========="
-  echo "❌ Some unknown words detected in spell check:"
-  cat spellcheck_detected.txt
-  echo "Fix them or add to 'hunspell_dict.txt' - spell check dictionary."
-  echo "=========="
-  exit 1
-fi
+for file in apps/*/data.yaml; do
+  echo "$file"
+  yq '.summary, .description' "$file" | tr '[:space:][:punct:]' '\n' | hunspell -d en_US -p ./hunspell_dict.txt -l | sort | uniq > spellcheck_detected.txt
+  if grep -rnwFf spellcheck_detected.txt $file; then
+    echo "=========="
+    echo "❌ Some unknown words detected in spell check ($file):"
+    cat spellcheck_detected.txt
+    echo "Fix them or add to 'hunspell_dict.txt' - spell check dictionary."
+    echo "=========="
+    exit 1
+  fi
+done
 echo "✅ Spell check OK"
