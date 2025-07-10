361e58b6a68ee1a16509c3ade544443b8c3b7542
Josef Hak <jhak@mirantis.com>
2025-07-10 11:05:22 +0200

    Add optional CRED_NAME env var to ./scripts/setup_provider_credential.sh

--- scripts/setup_provider_credential.sh
+++ scripts/setup_provider_credential.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+if [[ -z "${CRED_NAME:-}" ]]; then
+    CRED_NAME="credential"
+fi
+
 if [[ "$TEST_MODE" == aws ]]; then
-    helm upgrade --install aws-credential oci://ghcr.io/k0rdent/catalog/charts/aws-credential \
+    helm upgrade --install aws-${CRED_NAME} oci://ghcr.io/k0rdent/catalog/charts/aws-credential \
         --version 0.0.1 \
         -n kcm-system
 
-    kubectl patch secret aws-credential-secret -n kcm-system -p='{"stringData":{"AccessKeyID":"'$AWS_ACCESS_KEY_ID'"}}'
-    kubectl patch secret aws-credential-secret -n kcm-system -p='{"stringData":{"SecretAccessKey":"'$AWS_SECRET_ACCESS_KEY'"}}'
+    kubectl patch secret aws-${CRED_NAME}-secret -n kcm-system -p='{"stringData":{"AccessKeyID":"'$AWS_ACCESS_KEY_ID'"}}'
+    kubectl patch secret aws-${CRED_NAME}-secret -n kcm-system -p='{"stringData":{"SecretAccessKey":"'$AWS_SECRET_ACCESS_KEY'"}}'
 elif [[ "$TEST_MODE" == azure ]]; then
     helm upgrade --install azure-credential oci://ghcr.io/k0rdent/catalog/charts/azure-credential \
         --version 0.0.1 \
@@ -15,9 +19,9 @@ elif [[ "$TEST_MODE" == azure ]]; then
         --set "spAppID=${AZURE_SP_APP_ID}" \
         --set "spTenantID=${AZURE_SP_TENANT_ID}"
 
-    kubectl patch secret azure-credential-secret -n kcm-system -p='{"stringData":{"clientSecret":"'$AZURE_SP_PASSWORD'"}}'
+    kubectl patch secret azure-${CRED_NAME}-secret -n kcm-system -p='{"stringData":{"clientSecret":"'$AZURE_SP_PASSWORD'"}}'
 else
-    helm upgrade --install adopted-credential oci://ghcr.io/k0rdent/catalog/charts/adopted-credential \
+    helm upgrade --install adopted-${CRED_NAME} oci://ghcr.io/k0rdent/catalog/charts/adopted-credential \
     --version 0.0.1 \
     -n kcm-system
 fi
