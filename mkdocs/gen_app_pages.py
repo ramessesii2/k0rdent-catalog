f1fad992af17f1d63d80fdfefa75899ea83caa23
Josef Hak <jhak@mirantis.com>
2025-08-01 07:12:26 +0200

    Add kcm v1.2.0 support

--- mkdocs/gen_app_pages.py
+++ mkdocs/gen_app_pages.py
@@ -16,7 +16,7 @@ allowed_tags = ['AI/Machine Learning', 'Application Runtime', 'Authentication', 
                 'Monitoring', 'Networking', 'Security', 'Serverless', 'Storage']
 allowed_support_types = ['Enterprise', 'Community', 'Partner']
 summary_chars_limit = 90
-valid_versions = ['v0.1.0', 'v0.2.0', 'v0.3.0', 'v1.0.0', 'v1.1.0', 'v1.1.1']
+valid_versions = ['v0.1.0', 'v0.2.0', 'v0.3.0', 'v1.0.0', 'v1.1.0', 'v1.1.1', 'v1.2.0']
 
 VERSION = os.environ.get('VERSION', 'v1.1.1')
 
@@ -220,6 +220,25 @@ def version2template_names(version: str) -> str:
             "remote_cluster": "remote-cluster-1-0-8",
             "vsphere_hosted_cp": "vsphere-hosted-cp-1-0-8",
             "vsphere_standalone_cp": "vsphere-standalone-cp-1-0-9"
+        }
+    if version == "v1.2.0":
+        return {
+            "adopted_cluster": "adopted-cluster-1-0-1",
+            "aws_eks": "aws-eks-1-0-3",
+            "aws_hosted_cp": "aws-hosted-cp-1-0-12",
+            "aws_standalone_cp": "aws-standalone-cp-1-0-12",
+            "azure_aks": "azure-aks-1-0-1",
+            "azure_hosted_cp": "azure-hosted-cp-1-0-14",
+            "azure_standalone_cp": "azure-standalone-cp-1-0-13",
+            "docker_hosted_cp": "docker-hosted-cp-1-0-2",
+            "gcp_gke": "gcp-gke-1-0-4",
+            "gcp_hosted_cp": "gcp-hosted-cp-1-0-14",
+            "gcp_standalone_cp": "gcp-standalone-cp-1-0-12",
+            "openstack_hosted_cp": "openstack-hosted-cp-1-0-3",
+            "openstack_standalone_cp": "openstack-standalone-cp-1-0-13",
+            "remote_cluster": "remote-cluster-1-0-12",
+            "vsphere_hosted_cp": "vsphere-hosted-cp-1-0-11",
+            "vsphere_standalone_cp": "vsphere-standalone-cp-1-0-11"
         }
     raise Exception(f"Unsupported version '{version}' found")
 
