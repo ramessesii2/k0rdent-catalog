018caf6f8d0ccf58061eca774d5e3900fb75e6ea
Josef Hak <jhak@mirantis.com>
2025-09-02 15:13:19 +0200

    Allow apps/<app>/hunspell_dict.txt files for spellcheck.sh

--- scripts/spellcheck.sh
+++ scripts/spellcheck.sh
@@ -3,14 +3,16 @@ set -euo pipefail
 
 input_dir=mkdocs
 echo "⏳ Running spell check for *.md files in 'mkdocs' folder"
+cat apps/*/hunspell_dict.txt hunspell_dict.txt > hunspell_dict_all.txt
 for file in apps/*/data.yaml; do
   echo "$file"
-  yq '.summary, .description' "$file" | tr '[:space:][:punct:]' '\n' | hunspell -d en_US -p ./hunspell_dict.txt -l | sort | uniq > spellcheck_detected.txt
+  spell_file="${file/data.yaml/hunspell_dict.txt}"
+  yq '.summary, .description' "$file" | tr '[:space:][:punct:]' '\n' | hunspell -d en_US -p ./hunspell_dict_all.txt -l | sort | uniq > spellcheck_detected.txt
   if grep -rnwFf spellcheck_detected.txt $file; then
     echo "=========="
     echo "❌ Some unknown words detected in spell check ($file):"
     cat spellcheck_detected.txt
-    echo "Fix them or add to 'hunspell_dict.txt' - spell check dictionary."
+    echo "Fix them or add to '$spell_file' - spell check dictionary."
     echo "=========="
     exit 1
   fi
